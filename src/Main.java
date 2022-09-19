1be30535b65fdb52d0764f76d0817bfa98395b59
Justin Fleischman <justinfleischman@justins-mbp-3.wifi.uc.edu>
2022-09-19 12:59:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+       double priPurchase = 5.75;
+       double salesTax = 0.05;
+       System.out.println("The price of the purchase is" + " " + priPurchase);
+       System.out.println("The sales tax is  " + salesTax);
+    }
+}
